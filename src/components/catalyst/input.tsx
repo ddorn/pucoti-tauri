d92a267d014fbc395fd682c534b12a890055e159
ddorn <diego.dorn@free.fr>
2026-01-24 12:47:46 +0100

    Restore original catalyst/input.tsx

--- src/components/catalyst/input.tsx
+++ src/components/catalyst/input.tsx
@@ -40,8 +40,9 @@ export const Input = forwardRef(function Input(
         // Basic layout
         'relative block w-full',
         // Background color + shadow applied to inset pseudo element, so shadow blends with border in light mode
-        // Hidden in dark mode (we're dark-mode only)
-        'before:hidden',
+        'before:absolute before:inset-px before:rounded-[calc(var(--radius-lg)-1px)] before:bg-white before:shadow-sm',
+        // Background color is moved to control and shadow is removed in dark mode so hide `before` pseudo
+        'dark:before:hidden',
         // Focus ring
         'after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:ring-transparent after:ring-inset sm:focus-within:after:ring-2 sm:focus-within:after:ring-blue-500',
         // Disabled state
